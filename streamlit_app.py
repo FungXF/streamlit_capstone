15bd1a16c3dcf2e717eebf0f8fc4df3606fde2cf
fungxf <fungxuefeng@gmail.com>
2022-11-10 15:02:29 +0800

    capstone

--- streamlit_app.py
+++ streamlit_app.py
@@ -364,28 +364,28 @@ if submit:
         colres1, colres2, = st.columns(2)
 
         with colres1:
-            st.image((output['image'][0]),width=300,)
+            st.image((image[0]),width=300,)
             st.markdown(f"[{(' '.join(names[0].split('_'))).title()}](%s)" % url[0])
 
         with colres2:
-            st.image((output['image'][1]),width=300,)
+            st.image((image[1]),width=300,)
             st.markdown(f"[{(' '.join(names[1].split('_'))).title()}](%s)" % url[1])
         
         colres3, colres4 = st.columns(2)
         with colres3:
-            st.image((output['image'][2]),width=300,)
+            st.image((image[2]),width=300,)
             st.markdown(f"[{(' '.join(names[2].split('_'))).title()}](%s)" % url[2])
         
         with colres4:
-            st.image((output['image'][3]),width=300,)
+            st.image((image[3]),width=300,)
             st.markdown(f"[{(' '.join(names[3].split('_'))).title()}](%s)" % url[3])
         
         colres5, colres6 = st.columns(2)
 
         with colres5:
-            st.image((output['image'][4]),width=300,)
+            st.image((image[4]),width=300,)
             st.markdown(f"[{(' '.join(names[4].split('_'))).title()}](%s)" % url[4])
 
         with colres6:
-            st.image((output['image'][5]),width=300,)
+            st.image((image[5]),width=300,)
             st.markdown(f"[{(' '.join(names[5].split('_'))).title()}](%s)" % url[5])
