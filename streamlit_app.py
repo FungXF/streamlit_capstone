066390fbb9de02f73988007680bd775ed47db1b9
fungxf <fungxuefeng@gmail.com>
2022-11-12 18:23:54 +0800

    captstone

--- streamlit_app.py
+++ streamlit_app.py
@@ -318,10 +318,10 @@ def add_bg_from_url():
              font-weight: bold;
              color:#5A5A5A;  
          }}
-         h1, h2 {{
+         h1, h2  {{
              color:#5A5A5A;
          }}
-         .css-1offfwp p {{
+         .css-1offfwp, .css-1fv8s86.e16nr0p34 {{
              font-size: 20px;
              font-weight: bold;
              color:#5A5A5A;         
